c026ccf801bb7eb0c3446d5b60e256bc004d88d9
Slava Sidorov <sidorov-vv3@mail.ru>
2024-07-06 01:45:04 +0700

    "Added Redux toolkit, react-redux, and immer dependencies; updated Cart and Header components to use Redux; added toggleCart action and cartSlice; updated main.jsx to use Provider from react-redux."

--- src/modules/Order/Order.jsx
+++ src/modules/Order/Order.jsx
@@ -13,6 +13,7 @@ export const Order = () => {
           <h2 className="order__title">Заказ оформлен!</h2>
           <p className="order__id">Ваш номер заказа: 971f365a-caa1-4cdb-9446-bad2eff047e1</p>
         </div>
+        <button className="order__close" type="button">×</button>
       </div>
     )
   }
